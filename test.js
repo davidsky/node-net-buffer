78190b4544ea58480c646f5105f9a0855c926307
david <espaiz@gmail.com>
2016-02-12 23:31:23 +0800

    benchmarking

--- test.js
+++ test.js
@@ -2,29 +2,30 @@ var encode= require('./index').encode
 var decode= require('./index').decode
 var net= require('net')
 
-var time, messages= 100000, data
+var time
+var messages= parseInt(process.argv[2]) || 100
+var received= 0
+var data= new Buffer('hello')
+
+function onMesssage(buffer)
+{
+	if( ++received===messages )
+	{
+		var diff = process.hrtime(time)
+		console.log( (''+buffer===''+data? 'ok': 'error'), 'sent and received', messages, 'messages in', diff[0]+'.'+diff[1], 'seconds')
+   		process.exit()
+	}
+}
 
 net.createServer(function(socket)
 {
-	var received= 0
-
-	socket.on('readable', decode(socket, function(buffer)
-	{
-		if( ++received===messages )
-		{
-			var diff = process.hrtime(time)
-			console.log('sent and received', messages,'messages in', diff[0]+'.'+diff[1], 'seconds')
-	   		console.log(''+buffer===''+data? 'ok': 'error')
-		}
-	}))
+	socket.on('readable', decode(socket, onMesssage))
 
 }).listen(8181)
 
 var client= net.connect({port: 8181}, function()
 {
 	time= process.hrtime()
-	data= undefined
-	// data= new Buffer('Hello World!')
 	for(var i= 0, max= messages; max>i; ++i)
 		client.write( encode(data) )
 });
